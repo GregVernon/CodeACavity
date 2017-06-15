12dbb96691338dbdefab0529ee350dc8df245ea1
GregVernon <greg.j.vernon@gmail.com>
2017-06-14 22:57:14 -0500

    Copy "Native" into "Built-in"
    For ease of tracking differences between "native" and "built-in"

---
+++ src/MATLAB/Built-in/computePSI.m
@@ -0,0 +1,12 @@
+function PSI = computePSI(PSI, OMEGA, h, NY, NX, tol)
+err = inf;
+while err > tol
+    err = 0;
+    PSI0=PSI;
+    for jj=2:NY-1
+        for ii=2:NX-1
+            PSI(jj,ii)=0.25*(PSI(jj+1,ii)+PSI(jj-1,ii)+PSI(jj,ii+1)+PSI(jj,ii-1)+h*h*OMEGA(jj,ii));
+            err = err + natmat_abs(PSI0(jj,ii) - PSI(jj,ii));
+        end
+    end
+end
---
+++ src/MATLAB/Built-in/lidCavity.m
@@ -0,0 +1,45 @@
+%%
+clear
+close all
+
+N = 20;
+NY = N;
+NX = N;
+
+Visc=.01;
+tmax = 10;
+dt_max = tmax / 1000;
+tol = 1e-9;
+PSI=zeros(NY,NX);
+OMEGA=zeros(NY,NX);
+U = zeros(NY,NX);
+V = zeros(NY,NX);
+VELOCITY = zeros(NY,NX);
+cREYNOLDS = zeros(NY,NX);
+w=zeros(NY,NX);
+h=1.0/(NY-1);
+t=0.0;
+pIter = 0;
+while t < tmax % start the time integration
+    pIter = pIter+1;
+    % Compute timestep
+    dt = delta_t(VELOCITY, cREYNOLDS, dt_max, h, NY, NX);
+    % Compute streamfunction
+    PSI = computePSI(PSI,OMEGA,h, NY, NX, tol);     
+    % Apply vorticity boundary conditions
+    OMEGA = applyBC_OMEGA(PSI, OMEGA, h, NY, NX);
+    % Compute vorticity    
+    OMEGA = computeOMEGA(PSI, OMEGA, dt, h, NY, NX, Visc);
+    % Compute Velocity
+    [U, V, VELOCITY, cREYNOLDS] = computeVELOCITY(PSI, h, NY, NX);
+    % Increment time value by timestep
+    t=t+dt;
+    %% plot
+    if pIter == 1e2
+        pIter = 0;
+        disp(['Time: ' num2str(t)])
+        subplot(131), contourf(rot90(fliplr(OMEGA))), axis('square'); colorbar% plot vorticity
+        subplot(132), contourf(rot90(fliplr(PSI))), axis('square'); colorbar% streamfunction
+        subplot(133), quiver(rot90(fliplr(U)),rot90(fliplr(V))), axis('square');axis([1 N 1 N]); drawnow % streamfunction
+    end
+end
