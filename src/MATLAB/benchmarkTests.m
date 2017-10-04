2d471c764891acbc3cb8bc6c84ec544954cb4d3a
GregVernon <greg.j.vernon@gmail.com>
2017-10-03 20:43:06 -0500

    Added benchmark tests

--- src/MATLAB/benchmarkTests.m
+++ src/MATLAB/benchmarkTests.m
@@ -1,43 +1,125 @@
 clear
 close all
 clc
-
+figure; hold on; 
+N = [10:1:100];
+%%
 cd Native
-FCN = @() lidCavity(20,10,1e-5,false);
-t(1) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    %     N = 2^n;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(1,kk) = timeit(FCN);
+    if kk == 1
+        p{1} = plot(N(1:kk),t(1,1:kk),'DisplayName','Native');
+        legend(p{1});
+    else
+        p{1}.XData = N(1:kk);
+        p{1}.YData = t(1,1:kk);
+    end
+    drawnow
+end
 cd ..
-
+%%
 cd Built-in
-FCN = @() lidCavity(20,10,1e-5,false);
-t(2) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(2,kk) = timeit(FCN);
+    if kk == 1
+        p{2} = plot(N(1:kk),t(2,1:kk),'DisplayName','Built-in');
+    else
+        p{2}.XData = N(1:kk);
+        p{2}.YData = t(2,1:kk);
+    end
+    drawnow
+end
 cd ..
 
+%%
 cd SimpleVectorization
-FCN = @() lidCavity(20,10,1e-5,false);
-t(3) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(3,kk) = timeit(FCN);
+    if kk == 1
+        p{3} = plot(N(1:kk),t(3,1:kk),'DisplayName','SimpleVectorization');
+    else
+        p{3}.XData = N(1:kk);
+        p{3}.YData = t(3,1:kk);
+    end
+    drawnow
+end
 cd ..
-
+%%
 % cd SimpleVectorization_GPU
-% FCN = @() lidCavity(20,10,1e-5,false);
+% FCN = @() lidCavity(N(ii),10,100,1e-5,false);
 % t(4) = timeit(FCN);
 % cd ..
-
+%%
 cd AdvancedVectorization_GPU
-FCN = @() lidCavity(20,10,1e-5,false);
-t(5) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(5,kk) = timeit(FCN);
+    if kk == 1
+        p{5} = plot(N(1:kk),t(5,1:kk),'DisplayName','AdvancedVectorization GPU');
+    else
+        p{5}.XData = N(1:kk);
+        p{5}.YData = t(5,1:kk);
+    end
+    drawnow
+end
 cd ..
-
+%%
 cd LinearAlgebra
-FCN = @() lidCavity(20,10,1e-5,false);
-t(6) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(6,kk) = timeit(FCN);
+    if kk == 1
+        p{6} = plot(N(1:kk),t(6,1:kk),'DisplayName','LinearAlgebra');
+    else
+        p{6}.XData = N(1:kk);
+        p{6}.YData = t(6,1:kk);
+    end
+    drawnow
+end
 cd ..
-
+%%
 cd LinearAlgebra_GPU
-FCN = @() lidCavity(20,10,1e-5,false);
-t(7) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(7,kk) = timeit(FCN);
+    if kk == 1
+        p{7} = plot(N(1:kk),t(7,1:kk),'DisplayName','LinearAlgebra GPU');
+    else
+        p{7}.XData = N(1:kk);
+        p{7}.YData = t(7,1:kk);
+    end
+    drawnow
+end
 cd ..
-
+%%
 cd LinearAlgebra_CPU_GPU
-FCN = @() lidCavity(20,10,1e-5,false);
-t(8) = timeit(FCN);
+kk = 0;
+for ii = 1:length(N)
+    kk = kk+1;
+    FCN = @() lidCavity(N(ii),10,100,1e-5,false);
+    t(8,kk) = timeit(FCN);
+    if kk == 1
+        p{8} = plot(N(1:kk),t(8,1:kk),'DisplayName','LinearAlgebra CPU GPU');
+    else
+        p{8}.XData = N(1:kk);
+        p{8}.YData = t(8,1:kk);
+    end
+    drawnow
+end
 cd ..
