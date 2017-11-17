4c43c085626841a7ada28e0b7c155069d0ee0d8d
GregVernon <greg.j.vernon@gmail.com>
2017-11-17 11:52:25 -0600

    Continue functionalization

--- src/MATLAB/SimpleVectorization/computeJacRelaxFactors.m
+++ src/MATLAB/SimpleVectorization/computeJacRelaxFactors.m
@@ -1,18 +1,19 @@
-function computeJacRelaxFactors(FDM)
+function w = computeJacRelaxFactors(FDM)
 D = eye(size(FDM)) .* diag(FDM,0);
 R = triu(FDM,1) + tril(FDM,-1);
 b = zeros(size(FDM,1),1);
 
 J = D\b - D\R;
 
-W = sym('w',[3 1]);
+W = sym('w',[size(J,1) 1]);
 
-for ii = 1:3
+eqn = cell(length(W),1);
+for ii = 1:length(W)
     eqn{ii} = (1-W(ii))*eye(size(J)) + W(ii)*J;
 end
 
 EQN = sym(1);
-for ii = 1:3
+for ii = 1:length(eqn)
     EQN = EQN * eqn{ii};
     % EQN = eqn{1} * eqn{2} * eqn{3}
 end
