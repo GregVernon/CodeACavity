b4371194f71eac73e5ca8b98f4b66c1981f848b2
GregVernon <greg.j.vernon@gmail.com>
2017-11-22 13:45:07 -0600

    Add richardson

---
+++ src/MATLAB/LinearAlgebra/richardson.m
@@ -0,0 +1,16 @@
+function x = richardson(FDM, b, tol, x0)
+weight = 0.2; %2/3;
+x = x0;
+% f = b - FDM*x;
+A = FDM.A;
+D = FDM.D; %speye(size(FDM)) .* spdiags(FDM,0);
+iD = FDM.iD;
+
+err = inf;
+while err > tol
+%     x = x + weight*f;
+%     f = b - FDM*x;
+    x = x - weight*(iD*(A*x - b));
+    res = b - A*x;
+    err = norm(res);
+end
