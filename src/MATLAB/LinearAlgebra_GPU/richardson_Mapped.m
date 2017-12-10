384ca2dc1c570594ea077ebc1c8cf26ce9e70d64
GregVernon <greg.j.vernon@gmail.com>
2017-12-10 17:07:36 -0600

    Mapped Richardson now properly computes true residual

--- src/MATLAB/LinearAlgebra_GPU/richardson_Mapped.m
+++ src/MATLAB/LinearAlgebra_GPU/richardson_Mapped.m
@@ -9,16 +9,23 @@ D = (-2*(dy^2 + dx^2));
 iD = 1/D;
 
 err = inf;
-b = reshape(b,NY,NX);
+bV = b;
+bM = reshape(b,NY,NX);
 x = reshape(x,NY,NX);
 jj = 2:NY-1;
 ii = 2:NX-1;
 while err > tol
-    x0 = x;
-    Ax = ((dx^2*x(jj+1,ii)) + (dx^2*x(jj-1,ii)) + (dy^2*x(jj,ii+1)) + (dy^2*x(jj,ii-1))) + (-2*(dy^2 + dx^2)*x(jj,ii));
-    x(jj,ii) = x(jj,ii) + weight*(iD*(b(jj,ii) - Ax));
-%     res = b - A*x;
-    err = sum(sum(abs(x-x0)));
+%     Ax = ((dx^2*x(jj+1,ii)) + (dx^2*x(jj-1,ii)) + (dy^2*x(jj,ii+1)) + (dy^2*x(jj,ii-1))) + (-2*(dy^2 + dx^2)*x(jj,ii));
+%     x(jj,ii) = x(jj,ii) + weight*(iD*(b(jj,ii) - Ax));
+    x(jj,ii) = arrayfun(@xArrayFun, x(jj,ii), x(jj+1,ii), x(jj-1,ii), x(jj,ii+1), x(jj,ii-1), bM(jj,ii), iD, weight, dx, dy);
+    res = bV - A*x(:);
+    err = norm(res);
 end
 
 x = reshape(x,NY*NX,1);
+end
+
+function xji = xArrayFun(xji, xjp1i, xjm1i, xjip1, xjim1, bji, iD, weight, dx, dy)
+    Ax = ((dx^2*xjp1i) + (dx^2*xjm1i) + (dy^2*xjip1) + (dy^2*xjim1)) + (-2*(dy^2 + dx^2)*xji);
+    xji = xji + weight*(iD*(bji - Ax));
+end
