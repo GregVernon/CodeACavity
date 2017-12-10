a97740afc3fee8cc1f4e729d97debbcbfe4ea168
GregVernon <greg.j.vernon@gmail.com>
2017-12-10 17:08:13 -0600

    Slightly faster velocity computation

--- src/MATLAB/LinearAlgebra_GPU/computeVELOCITY.m
+++ src/MATLAB/LinearAlgebra_GPU/computeVELOCITY.m
@@ -6,29 +6,39 @@ V = zeros(NY,NX,'gpuArray');
 
 jj = 2:NY-1;
 ii = 2:NX-1;
-ji = jj + (ii - 1).*(NY);
+JJ = reshape(repmat(jj',1,NX-2),(NY-2)*(NX-2),1);
+II = reshape(repmat(ii,NY-2,1),(NY-2)*(NX-2),1);
+ji = JJ + (II - 1).*(NY);
 
 jj = [2:NY-1]+1;
 ii = [2:NX-1];
-jp1i = jj + (ii - 1).*(NY);
+JJ = reshape(repmat(jj',1,NX-2),(NY-2)*(NX-2),1);
+II = reshape(repmat(ii,NY-2,1),(NY-2)*(NX-2),1);
+jp1i = JJ + (II - 1).*(NY);
 
 jj = [2:NY-1]-1;
 ii = [2:NX-1];
-jm1i = jj + (ii - 1).*(NY);
+JJ = reshape(repmat(jj',1,NX-2),(NY-2)*(NX-2),1);
+II = reshape(repmat(ii,NY-2,1),(NY-2)*(NX-2),1);
+jm1i = JJ + (II - 1).*(NY);
 
 jj = [2:NY-1];
 ii = [2:NX-1]+1;
-jip1 = jj + (ii - 1).*(NY);
+JJ = reshape(repmat(jj',1,NX-2),(NY-2)*(NX-2),1);
+II = reshape(repmat(ii,NY-2,1),(NY-2)*(NX-2),1);
+jip1 = JJ + (II - 1).*(NY);
 
 jj = [2:NY-1];
 ii = [2:NX-1]-1;
-jim1 = jj + (ii - 1).*(NY);
+JJ = reshape(repmat(jj',1,NX-2),(NY-2)*(NX-2),1);
+II = reshape(repmat(ii,NY-2,1),(NY-2)*(NX-2),1);
+jim1 = JJ + (II - 1).*(NY);
 
 % U(jj,ii) =  arrayfun(@uArrayFun, PSI(jj+1,ii), PSI(jj-1,ii), dy); %(PSI(jj+1,ii) - PSI(jj-1,ii)) / (2*dy);
 % V(jj,ii) =  arrayfun(@vArrayFun, PSI(jj,ii+1), PSI(jj,ii-1), dx); %-(PSI(jj,ii+1) - PSI(jj,ii-1)) / (2*dx);
 U(ji) =   (PSI(jp1i) - PSI(jm1i)) / (2*dy); % arrayfun(@uArrayFun, PSI(jp1i), PSI(jm1i), dy);
 V(ji) =  -(PSI(jip1) - PSI(jim1)) / (2*dx); % arrayfun(@vArrayFun, PSI(jip1), PSI(jim1), dx);
-VELOCITY =  sqrt(U.^2 + V.^2);
+VELOCITY =  arrayfun(@hypot,U,V); %sqrt(U.^2 + V.^2);
 cREYNOLDS = abs(U) + abs(V);
 
 end
