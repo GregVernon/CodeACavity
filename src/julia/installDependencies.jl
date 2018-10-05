a4b062dc87ce1aca86eae044c186aa66bd189270
Greg Vernon <greg.j.vernon@gmail.com>
2018-10-04 23:43:47 -0500

    Dependency installer and pre-build

---
+++ src/julia/installDependencies.jl
@@ -0,0 +1,14 @@
+import Pkg
+Pkg.add("IterativeSolvers")
+Pkg.add("SparseArrays")
+Pkg.add("Plots")
+Pkg.add("PyPlot")
+Pkg.add("PyCall")
+Pkg.add("LaTeXStrings")
+
+import IterativeSolvers
+import SparseArrays
+import Plots
+import PyPlot
+import PyCall
+import LaTeXStrings
